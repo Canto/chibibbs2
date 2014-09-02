8d8187b50637b54c82ba786a853896e5022df43e
canto87-mac <canto87@naver.com>
2014-09-02 13:59:28 +0900

    modify index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ header ('Content-type: text/html; charset=UTF-8');
 session_set_cookie_params(0);
 session_start();
 define("__CHIBI__",time());
-
+define("__XE__",true);
+error_reporting(E_ALL ^ E_NOTICE);
 /* check database config file */
 if(is_file('data/config/db.config.php')==false){
 	echo "<script type=\"text/javascript\">location.href=\"modules/install/install.php\";</script>";
@@ -14,34 +15,40 @@ if(is_file('data/config/db.config.php')==false){
 
 /* Set BBS */
 require_once "data/config/db.config.php";
-require_once "lang/lang.ko.php";
-require_once "classes/Develop.class.php";
-require_once "classes/DB.class.php";
-require_once "classes/Paging.class.php";
-require_once "classes/Board.class.php";
-require_once "classes/Install.class.php";
-require_once "classes/Display.class.php";
+require_once "lang/".$LANG;
+require_once "classes/classes.php";
+/* chibi version */
+$chibi_ver = "2.0";
 
 /* Set DB */
-$db = new DB();
+$db = new CDB();
 $db->set($DBINFO);
 /* Check Installed Board */
 $install = new Install();
-$install->install_check($db->conn());
+if($install->install_check($db->conn())==false)
+{
+echo "<script type=\"text/javascript\">location.href=\"modules/install/install.php\";</script>";
+exit;
+}
 
+if($_GET['mid']=="admin"){
+	require_once "modules/admin/admin.php";
+}else{
 /* Check Board ID */
 if(!isset($_GET['cid']) || empty($_GET['cid'])){ /* If $_GET['cid'] is empty */
-	echo "<script language=\"javascript\">alert(\"".$lang->cid_empty."\");</script>";
+	echo "<script language=\"javascript\">alert(\"".$clang->cid_empty."\");</script>";
 	exit;
 }
 
 /* Set Display Function */
 $display = new Display();
+/* Set Template Function */
+$tpl = new Template();
 /* Set board info, if have a cid. */
 $bbs = new Board();
 $board = $bbs->select($_GET['cid'],$db->conn());
 /* Set board skin info , if have a cid */
-$skin = $bbs->gets_skin();
+$skin = $bbs->skin($_GET['cid'],$db->conn());
 
 /* include user function file, if user function file exists */
 if(file_exists("skin/".$board->skin."/user.fn.php")) include_once 'skin/'.$board->skin.'/user.fn.php';
@@ -51,7 +58,7 @@ $bbs->ip_check();
 
 /* If board is empty  */
 if(!isset($_GET['cid']) || empty($board->cid)==true){
-	echo "<script language=\"javascript\">alert(\"".$lang->board_empty."\");</script>";
+	echo "<script language=\"javascript\">alert(\"".$clang->board_empty."\");</script>";
 	exit;
 }
 if(!empty($_GET['search'])) $search = $_GET['search'];
@@ -84,15 +91,19 @@ $page->setUrl('cid='.$board->cid);
 if(empty($_GET['search'])==false) $page->setUrl('search='.$_GET['search']);
 if(empty($_GET['keyword'])==false) $page->setUrl('keyword='.$_GET['keyword']);
 if($_GET['page']==1) $start = 0;
+else $start = ($_GET['page']-1) * $board->op->pic_page;
 $page->setDisplay('prev_btn','<');
 $page->setDisplay('next_btn','>');
-$page->setDisplay('start_btn','>>');
+$page->setDisplay('start_btn','<<');
 $page->setDisplay('end_btn','>>');
 $page->setDisplay('full');
 if($total != 0) $paging = $page->showPage();
 else $paging = '';
 
+if(!empty($_GET['act'])) $act = $_GET['act'];
+else $act = '';
+
 /* include skin file */
 require_once "skin/".$board->skin."/index.layout.php";
-
+}
 ?>
